7e10de198d062064d20ae2030ef548498244db56
JITHESH <jitheshkramesh@hotmail.com>
2019-03-11 11:39:56 +0400

    Change titles and styling on homepage

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+    data: any;
+    constructor(private newsService: NewsService,private router:Router) { }
+    ngOnInit() {
+        this.newsService
+            .getData('everything?q=bitcoin&from=2019-02-11')
+            .subscribe(data => {
+                console.log(data);
+                this.data = data;
+            });
+    }
+
+    onGoToNewsSinglePage(article) {
+        this.newsService.currentArticle = article;
+        this.router.navigate(['news-single'])
+    }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    apiUrl: 'https://newsapi.org/v2',
+    apiKey:'e02ca8f73a1a4db9ab11406395515051'
 };
 
 /*
